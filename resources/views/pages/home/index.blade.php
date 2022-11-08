12027aa4735eede5175b6a1a6ceaa8007b4669f2
Maneka123 <maneka611997@gmail.com>
2022-11-08 20:03:05 +0530

    second commit.created navigation bar

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+<div class="row">
+<div class="col-lg-12 text-center">
+<h1 class="page-title">Student List</h1>
+
+</div>
+</div>
+</div>
+
+@endsection
+
+@push('css')
+<style>
+.page-title{
+    padding : 10vh;
+    font-size:5rem;
+    color:green;
+
+}
+
+</style>
+@endpush
